552e66ee58ed456f23109e62131148923fc9f3be
Yuri Romeu <yuriromeu2007@gmail.com>
2026-02-25 10:37:07 -0300

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+const name = 'Yuri';
+</script>
+
+<template>
+     <h1>Dashboard User - {{ name }}</h1>
+</template>
+
+<style scoped>
+h1 {
+     color: aquamarine;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import './assets/theme/theme.css';
+
+createApp(App).mount('#app');
