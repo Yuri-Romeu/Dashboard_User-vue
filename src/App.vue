95b8c2e4c6920ba508d7fcb0ba46d4368ade7ff2
Yuri Romeu <yuriromeu2007@gmail.com>
2026-02-25 11:14:13 -0300

    componente searchUser + lucideIcon

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 // importações
 import Menu from './components/menu.vue';
+import SearchUser from './components/searchUser.vue';
 </script>
 
 <template>
      <Menu />
+
+     <div class="container">
+          <SearchUser />
+     </div>
 </template>
 
-<style scoped>
-h1 {
-     color: aquamarine;
-}
-</style>
+<style scoped></style>
---
+++ src/components/searchUser.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { Search } from 'lucide-vue-next';
+</script>
+
+<template>
+     <div class="containerSearchUser">
+          <h1 class="title">List of Users</h1>
+
+          <div class="searchUserContant">
+               <Search
+                    class="searchUserContantIcon"
+                    :color="'rgba(128, 128, 128, 0.8)'"
+                    :size="20"
+               />
+               <input class="inputSearchUser" type="text" placeholder="Search users..." />
+          </div>
+
+          <div class="filter">
+               <select>
+                    <option value="all">All</option>
+                    <option value="female">Female</option>
+                    <option value="male">Male</option>
+               </select>
+          </div>
+     </div>
+</template>
+
+<style scoped>
+.containerSearchUser {
+     margin: 14px 0;
+}
+
+.searchUserContant {
+     position: relative;
+}
+
+.searchUserContantIcon {
+     position: absolute;
+     top: 50%;
+     left: 12px;
+     transform: translateY(-50%);
+     pointer-events: none;
+}
+
+.inputSearchUser {
+     width: 40%;
+     padding: 12px 16px 12px 42px;
+     margin: 10px 0;
+     border-radius: 4px;
+     border: 1px solid rgba(128, 128, 128, 0.356);
+
+     &::placeholder {
+          color: rgba(128, 128, 128, 0.8);
+     }
+}
+
+.filter {
+     margin: 5px;
+}
+
+select {
+     padding: 4px 7px;
+     border-radius: 4px;
+     border: 1px solid rgba(128, 128, 128, 0.356);
+}
+</style>
